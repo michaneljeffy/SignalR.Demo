b47e218e9b6877e3d01110668baee77a3689941e
jinghong <jinghong@in66.com>
2017-10-24 21:01:28 +0800

    merge

---
+++ Demo1/Hubs/StockTickerHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Demo1.Common;
+using Demo1.Model;
+
+namespace Demo1.Hubs
+{
+    [HubName("stockTickerMini")]
+    public class StockTickerHub : Hub
+    {
+        private readonly StockTicker  _stockTicker;
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public StockTickerHub() : this(StockTicker.Instance ) { }
+
+        public StockTickerHub (StockTicker stockTicker)
+        {
+            _stockTicker = stockTicker;
+        }
+
+        public IEnumerable<Stock> GetAllStocks()
+        {
+            return _stockTicker.GetAllStocks();
+        }
+    }
+}
---
+++ Demo1/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR.Owin;
+
+[assembly:OwinStartup (typeof(Demo1.Startup))]
+namespace Demo1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
